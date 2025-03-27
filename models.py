d58c5a502f8e51bbef529d6d4b37fbe2607dc06a
Everton Vaszelewski <50892923+Vaszelewski@users.noreply.github.com>
2025-03-27 20:07:22 -0300

    MODELS

--- models.py
+++ models.py
@@ -18,8 +18,8 @@ for model in modelsXg:
 
 
 
-##model_uri = "file:///C:/Users/Everton Vaszelewski/Downloads/Trabalho_ECD15_MLOps-master/mlruns/1/b7096dbb02f145e78ff54af0ffe295b5/artifacts/random_forest_model"
-##mlflow.register_model(model_uri, "RandomForestGridSearch")
+model_uri = "file:///C:/Users/Everton Vaszelewski/Downloads/Trabalho_ECD15_MLOps-master/mlruns/1/b7096dbb02f145e78ff54af0ffe295b5/artifacts/random_forest_model"
+mlflow.register_model(model_uri, "RandomForestGridSearch")
 
 model_uriXG = "file:///C:/Users/Everton Vaszelewski/Downloads/Trabalho_ECD15_MLOps-master/mlruns/1/6f93e7e89ad2497090694d8244befd59/artifacts/xgboost_grid_search"
 mlflow.register_model(model_uriXG, "XGBoostGridSearch")
