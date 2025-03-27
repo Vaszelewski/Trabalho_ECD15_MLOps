32d90c54f13324fd6604397d12c1746e8c802402
Everton Vaszelewski <50892923+Vaszelewski@users.noreply.github.com>
2025-03-27 20:03:50 -0300

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-import mlflow
-from mlflow.tracking import MlflowClient
-
-client = MlflowClient(tracking_uri="sqlite:///mlflowRf.db")
-
-# Verifica o modelo registrado
-models = client.search_registered_models()
-for model in models:
-    print(f"Modelo: {model.name}, Local: {model.latest_versions}")
